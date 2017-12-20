f3087a3fd21718c1d300ad91b979c572de659ecd
Alessandra Cabassi CS <ac2051@cam.ac.uk>
2017-12-20 16:08:06 +0000

    add import pchisq

--- R/fdcov.R
+++ R/fdcov.R
@@ -1,27 +1,27 @@
 #' Analysis of Covariance Operators.
 #'
-#' \code{fdcov} provides a variety of tools for the analysis of 
+#' \code{fdcov} provides a variety of tools for the analysis of
 #' covariance operators.
 #'
-#' This package contains a collection of tools for performing 
+#' This package contains a collection of tools for performing
 #' statistical inference on functional data specifically through
 #' an analysis of the covariance structure of the data.  It includes
-#' two methods for performing a k-sample test for equality 
+#' two methods for performing a k-sample test for equality
 #' of covariance in \code{ksample.perm} and \code{ksample.com}.
 #' For supervised and unsupervised learning,
-#' it contains a method to classify functional data with 
-#' respect to each category's covariance operator in 
-#' \code{classif.com}, 
+#' it contains a method to classify functional data with
+#' respect to each category's covariance operator in
+#' \code{classif.com},
 #' and it contains a method to cluster functional data, \code{cluster.com},
 #' again based on the covariance structure of the data.
-#' 
+#'
 #' The current version of this package assumes that all functional
 #' data is sampled on the same grid at the same intervals.  Future
 #' updates are planned to allow for the below methods to interface
 #' with the \code{fda} package and its functional basis representations
 #' of the data.
-#' 
-#' @author 
+#'
+#' @author
 #' Alessandra Cabassi \email{alessandra.cabassi@mail.polimi.it},
 #' Adam B Kashlak \email{kashlak@ualberta.ca}
 #' @references
@@ -30,17 +30,17 @@
 #'   inequalities: k-sample tests, classification, and clustering via
 #'   Rademacher complexities", April, 2016 (in review)
 #'
-#'   Pigoli, Davide, John AD Aston, Ian L Dryden, and Piercesare Secchi. 
-#'   "Distances and inference for covariance operators." 
+#'   Pigoli, Davide, John AD Aston, Ian L Dryden, and Piercesare Secchi.
+#'   "Distances and inference for covariance operators."
 #'   Biometrika (2014): 101(2):409–422.
 #'
-#'   Panaretos, Victor M., David Kraus, and John H. Maddocks. 
-#'   "Second-order comparison of Gaussian random functions and the 
-#'   geometry of DNA minicircles." Journal of the American Statistical 
+#'   Panaretos, Victor M., David Kraus, and John H. Maddocks.
+#'   "Second-order comparison of Gaussian random functions and the
+#'   geometry of DNA minicircles." Journal of the American Statistical
 #'   Association 105.490 (2010): 670-682.
 #' @importFrom grDevices dev.off postscript setEPS
-#' @importFrom stats cov qnorm rbinom rgamma sd
-#' @importFrom utils setTxtProgressBar txtProgressBar 
+#' @importFrom stats cov qnorm rbinom rgamma sd pchisq
+#' @importFrom utils setTxtProgressBar txtProgressBar
 #'
 "_PACKAGE"
 #> [1] "_PACKAGE"
