af0545881c5875cea8d633e776c849c482c249cd
Alessandra Cabassi <ac2051@cam.ac.uk>
2018-06-15 01:13:53 +0200

    add permutation type "isnps"
    first version

--- R/ksamp_perm.R
+++ R/ksamp_perm.R
@@ -1,9 +1,9 @@
 #' Multiple-sample permutation test for the equality of covariance operators of functional data
-#' 
+#'
 #' The method performs a test for the equality of the covariance operators of multiple data samples.
 #' It can also perform all of the pairwise comparisons between the groups and compute a p-value for each of them.
-#' This feature is useful when the global null hypothesis is rejected, so one may want to find out which samples have different covariances. 
-#' 
+#' This feature is useful when the global null hypothesis is rejected, so one may want to find out which samples have different covariances.
+#'
 #' @param dat n X p data matrix of n samples of p long vectors.
 #' @param grp n long vector of group labels.
 #' @param iter Number of permutations. Defaults to 1000.
@@ -14,34 +14,34 @@
 #' @param part If FALSE, the function computes only the global p-value; otherwise it computes also all the p-values corresponding to the pairwise comparisons. Defaults to FALSE.
 #' @param cent If FALSE, the mean functions of the groups are supposed to be different, therefore data are centred before performing the test. Defaults to FALSE.
 #' @param load Boolean flag, which if TRUE, prints a loading bar.
-#' 
+#'
 #' @return If \code{part} is set to FALSE, the output is the p-value associated to the global test. If \code{part} is TRUE, the function returns also all the p-values of the pairwise comparisons.
-#' 
+#'
 #' @author Alessandra Cabassi \email{alessandra.cabassi@mail.polimi.it}
-#' 
-#' @references 
-#'   Cabassi, A., Pigoli, D., Secchi, P., Carter, P. A. (2017). 
-#'   Permutation tests for the equality of covariance operators of 
-#'   functional data with applications to evolutionary biology. 
+#'
+#' @references
+#'   Cabassi, A., Pigoli, D., Secchi, P., Carter, P. A. (2017).
+#'   Permutation tests for the equality of covariance operators of
+#'   functional data with applications to evolutionary biology.
 #'   Electron. J. Statist. 11(2), pp.3815--3840.
 #'
-#'   Pigoli, Davide, John A. D. Aston, Ian L. Dryden, and 
-#'   Piercesare Secchi (2014). "Distances and inference for 
+#'   Pigoli, Davide, John A. D. Aston, Ian L. Dryden, and
+#'   Piercesare Secchi (2014). "Distances and inference for
 #'   covariance operators." Biometrika: 101(2):409–422.
-#' 
-#' @examples 
+#'
+#' @examples
 #' \dontrun{
 #' ## Phoneme data
-#' 
+#'
 #' library(fdcov)
 #' library(fds)
-#' 
-#' # Create data set 
+#'
+#' # Create data set
 #' data(aa); data(ao); data(dcl);data(iy);data(sh)
 #' dat = cbind(aa$y[,1:20],ao$y[,1:20],dcl$y[,1:20],iy$y[,1:20],sh$y[,1:20])
 #' dat = t(dat)
 #' grp = c(rep(1,20),rep(2,20),rep(3,20),rep(4,20),rep(5,20))
-#' 
+#'
 #' # Test the equality of the covariance operators
 #' p = ksample.perm(dat, grp, iter=100, part = TRUE)
 #' p$global # global p-value
@@ -50,13 +50,13 @@
 #' \dontshow{
 #' library(fdcov)
 #' library(fds)
-#' 
-#' # Create data set 
+#'
+#' # Create data set
 #' data(aa); data(ao); data(dcl);data(iy);data(sh)
 #' dat = cbind(aa$y[,1:20],ao$y[,1:20],dcl$y[,1:20],iy$y[,1:20],sh$y[,1:20])
 #' dat = t(dat)
 #' grp = c(rep(1,20),rep(2,20),rep(3,20),rep(4,20),rep(5,20))
-#' 
+#'
 #' # Test the equality of the covariance operators
 #' p = ksample.perm(dat,grp,iter=2)
 #' }
@@ -69,27 +69,28 @@ ksample.perm = function(dat, grp, iter = 1000, perm = 'sync', dist = 'sq', adj =
     C = length(table_groups)  # number of groups
     if(C < 2) stop('The number of groups must be at least 2.')
     if(C == 2) return(list(global = twosample.perm(dat, grp, iter, dist, load)))
-       
+
     ### Step 1: data centring
-  
+
     if(cent == FALSE){
         nonalign_dat = dat
         for(i in 1:length(grp)){
             dat[i,] = nonalign_dat[i,]-colMeans(nonalign_dat[grp == grp[i],],na.rm=TRUE)
         }
     }
-     
+
     ### Steps 2, 3 and 4: apply iter permutations and compute the test statistic for each permuted data set
-    
+
     T = switch (perm,
                     sync = perm.sync(dat, grp, iter, dist, load),
                     pool = perm.pool(dat, grp, iter, dist, load),
-                    stop('The selected permutation strategy is not available'))  
-  
+                    isnps = perm.isnps(dat, grp, iter, dist, load),
+                    stop('The selected permutation strategy is not available'))
+
     ### Global p-values
-  
+
     if (comb == 'tipp' | comb == 'fish' | comb == 'lipt' | adj == FALSE) P = perm.t2p(T)
-    
+
     P.glob = switch (comb,
                         tipp = mean(min(P[1,])       >= apply(P[-1,],1,min)),
                         maxT = mean(max(T[1,])       <= apply(T[-1,],1,max)),
@@ -97,9 +98,9 @@ ksample.perm = function(dat, grp, iter = 1000, perm = 'sync', dist = 'sq', adj =
                         fish = mean(comb.fish(P[1,]) <= apply(P[-1,],1,comb.fish)),
                         lipt = mean(comb.lipt(P[1,]) <= apply(P[-1,],1,comb.lipt)),
                         stop('The selected combining function is not available'))
-          
+
     ### Partial p-values
-    
+
     if(part == TRUE){
         if(adj == TRUE){ # Corrected p-values
             P.part = switch (comb,
@@ -110,7 +111,7 @@ ksample.perm = function(dat, grp, iter = 1000, perm = 'sync', dist = 'sq', adj =
                                 lipt = FWE.clos(P, comb, load))
         }else{P.part = matrix(P[1,],nrow = C*(C-1)/2,ncol = 1) # Raw p-values
         }
-        
+
         # Group names definition
         name = character()
         cont = 1
@@ -120,11 +121,11 @@ ksample.perm = function(dat, grp, iter = 1000, perm = 'sync', dist = 'sq', adj =
                 cont = cont+1
             }
         }
-        
+
         P.part = data.frame(dist = T[1,],p_value = P.part, signif = perm.sig(P.part))
         rownames(P.part) = name
-       
+
         return(list(global = P.glob,partial = P.part))
     }
-    else{return(list(global = P.glob))} 
+    else{return(list(global = P.glob))}
 }
---
+++ R/perm_isnps.R
@@ -0,0 +1,91 @@
+# Synchronised permutations of a data set
+#
+# @param dat n x p data set matrix
+#
+# @return This function returns a matrix containing the partial test statistics of each pairwise comparison for each permutation applied to the data set
+#
+# @author Alessandra Cabassi \email{alessandra.cabassi@mail.polimi.it}
+#
+
+perm.isnps = function(dat, grp, iter, dist, load = FALSE){
+
+    table_groups = table(grp) # groups table
+
+    for(i in length(table_groups)){
+        if(table_groups[i]!=table_groups[1])
+            stop("All groups must be of the same size in order to apply ISNPS permutations.")
+    }
+
+    n = (dim(dat)[1])/C
+
+    C = length(table_groups)  # number of groups
+    K = C*(C-1)/2             # number of partial tests
+    p = dim(dat)[2]           # number of samplings per function
+
+    T = array(5,dim = c((iter+1),K)) # test statistics vector initialisation
+
+    ### Step 2: compute test statistic
+
+    cont_pair = 1
+    for(i in 1:(C-1)){
+        for(j in (i+1):C){ # for each pair of groups
+            # compute test statistic for initial data
+            T[1,cont_pair] = distCov(cov(dat[grp==i,],use='pairwise'),cov(dat[grp==j,],use='pairwise'),dist)
+            cont_pair = cont_pair+1
+        }
+    }
+
+    ### Steps 3 and 4: apply iter permutations and compute the test statistic for each permuted data set
+
+    # Apply permutation to each dataset separately
+    cont_perm = 1
+    for(first_perm in 1:ceiling(iter/100)){
+
+        if(load) pb = txtProgressBar(min = 0, max = iter, style = 3) # create progress bar
+
+        for(i in 1:C){
+            orig_grp_i = dat[grp==i,]
+            perm_dat[grp==i,] = orig_grp_i[sample(n)]
+        }
+
+        # Build pseudomatrix
+
+        X = array(0,dim = c((2*n),p,K)) # matrix 2*n X p X number of groups
+        cont_pseudo = 1
+        for(i in 1:(C-1)){ # for each pair of groups
+            for(j in (i+1):C){
+                X[,,cont_pseudo] = rbind(perm_dat[grp==i,],perm_dat[grp==j,]) # fill in the matrix
+                cont_pseudo = cont_pseudo+1
+            }
+        }
+
+
+        # Apply 'iter' permutations to the pseudomatrix and compute test statistics
+
+        for(bb in 2:(100)){
+            exchanges = rbinom(n, size, prob) # select permutation
+            for(pota in 1:n){
+                if(exchanges[pota]==0){
+                    X.perm[pota,,] = X[pota,,]
+                    X.perm[pota+n,,] = X[pota,,]
+                }else{
+                    X.perm[pota,,] = X[pota+n,,]
+                    X.perm[pota+n,,] = X[pota,,]
+                }
+            }
+            cont_pair = 1
+            for(i in 1:(C-1)){ # for each pair of groups
+                for(j in (i+1):C){ # compute test statistic for the permuted dataset
+                    T[bb,cont_pair] = distCov(cov(X.perm[c(1:n),,cont_pair],use = 'pairwise'),cov(X.perm[-c(1:n),,cont_pair],use = 'pairwise'),dist)
+                    cont_pair = cont_pair+1
+                }
+            }
+            cont_perm = cont + 1
+        } # end iter
+        if(load) setTxtProgressBar(pb, bb-1) # update progress bar
+    }
+
+    if(load) close(pb) # close progress bar
+
+    return(T)
+}
